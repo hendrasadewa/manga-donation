9ad267db24863d317462e59bd7d4f2be6f0552b2
hendra-sadewa <hendra@stockbit.com>
2024-02-09 01:01:17 +0700

    add SEO

---
+++ src/lib/basicInfo.ts
@@ -0,0 +1,65 @@
+export const basicInfo = {
+	author: 'Hendra Sadewa',
+	title: 'Manga Donation',
+	twitter: '@sadevva_',
+	icon: '/icon.svg',
+	logo: '/logo.svg',
+	descriptions: ['Manga Donation from Hendrasadewa'].join(' '),
+	keywords: [
+		'Manga',
+		'Manga Donation',
+		'Jabodetabek',
+		'Web',
+		'Web dev',
+		'Web developer',
+		'Web developer Lead',
+		'Web developer Indonesia',
+		'Web engineer',
+		'Web engineer Lead',
+		'Web development',
+		'Web design',
+		'Developer indonesia',
+		'Application development',
+		'Software engineer',
+		'Senior Software engineer',
+		'Lead Software engineer',
+		'Software developer',
+		'Senior software developer',
+		'Lead Software developer',
+		'Indie developer',
+		'React developer',
+		'React.js developer'
+	].join(', ')
+};
+
+export const occupations = [
+	'A passionate web engineer 👨🏻‍💻',
+	'A seasoned writer 📇',
+	'A React.js addict ⚛️',
+	'A gig rat 🙌',
+	'A cat lover 🐈',
+	'An #odapi survivor 🩸',
+	'A tarnished 🗡',
+	'A weird music enjoyer 🎧'
+];
+
+export const socials = [
+	{
+		name: 'Twitter',
+		url: 'https://twitter.com/@sadevva_',
+		username: '@sadevva_',
+		icon: '/icons/twitter.svg'
+	},
+	{
+		name: 'LinkedIn',
+		url: 'https://linkedin.com/in/hendra-sadewa/',
+		username: 'hendra-sadewa',
+		icon: '/icons/linkedin.svg'
+	},
+	{
+		name: 'GitHub',
+		url: 'https://github.com/hendrasadewa',
+		username: 'hendrasadewa',
+		icon: '/icons/github.svg'
+	}
+];
---
+++ src/lib/components/SEO.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { basicInfo } from '$lib/basicInfo';
+
+	export let title = basicInfo.title;
+	export let description = basicInfo.descriptions;
+	export let keywords = basicInfo.keywords;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+
+	<meta name="og:title" content={title} />
+	<meta name="og:description" content={description} />
+	<meta name="og:keywords" content={keywords} />
+	<meta name="og:type" content="article" />
+	<meta name="og:url" content="https://donate-manga.hendrasadewa.com" />
+	<meta name="og:site_name" content={title} />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:creator" content={basicInfo.twitter} />
+	<meta name="twitter:site" content={basicInfo.twitter} />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+</svelte:head>
