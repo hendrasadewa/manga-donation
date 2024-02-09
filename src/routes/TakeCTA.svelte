6075d158144787dd48c1a05c7add7b56cebaef98
hendra-sadewa <hendra@stockbit.com>
2024-02-09 12:05:58 +0700

    fix the taken ui color

--- src/routes/TakeCTA.svelte
+++ src/routes/TakeCTA.svelte
@@ -14,14 +14,18 @@
 	url.searchParams.append('text', template);
 </script>
 
-<div class="flex items-center justify-center rounded-br py-1 bg-zanah-600">
-	{#if isAvailable}
+{#if isAvailable}
+	<div class="flex items-center justify-center rounded-br py-1 bg-zanah-600">
 		<a href={url.toString()}>
 			<span class="text-xs text-white">
 				Click here to ask @{USER}
 			</span>
 		</a>
-	{:else}
-		<span class="text-xs text-gray-500 italic">Taken</span>
-	{/if}
-</div>
+	</div>
+{:else}
+	<div class="flex items-center justify-center rounded-br py-1 bg-zanah-50">
+		<a href={url.toString()}>
+			<span class="text-xs text-gray-500 italic">Taken</span>
+		</a>
+	</div>
+{/if}
